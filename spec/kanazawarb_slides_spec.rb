c26e47e2baf4485f658b68302f23389b83b89185
kkabetani <k.kabetani@gmail.com>
2014-02-26 21:14:30 +0900

    fix convert_to_original_url for many times redirect

--- spec/kanazawarb_slides_spec.rb
+++ spec/kanazawarb_slides_spec.rb
@@ -17,12 +17,35 @@ describe KanazawarbSlides do
   end
 
   describe "omitted url" do
-    tco_url = "https://t.co/DyfjrVazCG"
-    original_url = "https://speakerdeck.com/kkabetani/kanazawa-dot-rb-slides"
 
-    it "convert to correct original url" do
-      expect(KanazawarbSlides.convert_to_original_url(tco_url)).to eq original_url
+    describe "one time redirect" do
+      tco_url = "https://t.co/DyfjrVazCG"
+      original_url = "https://speakerdeck.com/kkabetani/kanazawa-dot-rb-slides"
+
+      it "convert to correct original url" do
+        expect(KanazawarbSlides.convert_to_original_url(tco_url)).to eq original_url
+      end
     end
 
+    describe "many times redirect" do
+      tco_url = "http://t.co/qPedglEadF"
+      original_url = "http://www.slideshare.net/tomokazu/textobjects-vims-elegant-function"
+
+      it "convert to correct original url" do
+        expect(KanazawarbSlides.convert_to_original_url(tco_url)).to eq original_url
+      end
+
+      it "raise exception for too deep" do
+        expect { KanazawarbSlides.convert_to_original_url(tco_url, 1) }.to raise_error(ArgumentError)
+      end
+    end
+
+    describe "not found response" do
+      tco_url = "http://kanazawarb.github.io/meetup/notfound.html"
+
+      it "return nil" do
+        expect(KanazawarbSlides.convert_to_original_url(tco_url)).to eq nil
+      end
+    end
   end
 end
