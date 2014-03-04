a305dcb385a6c232280bb882fa40e584a057bf27
kkabetani <k.kabetani@gmail.com>
2014-03-04 19:40:32 +0900

    refactor rspec

--- spec/kanazawarb_slides_spec.rb
+++ spec/kanazawarb_slides_spec.rb
@@ -7,44 +7,44 @@ describe KanazawarbSlides do
   end
 
   describe "correct urls" do
-    urls = KanazawarbSlides.collect
+    before(:all) { @urls = KanazawarbSlides.collect }
 
-    it { expect(urls.size).to be > 0 }
+    it { expect(@urls.size).to be > 0 }
 
     it "uniq urls" do
-      expect(urls.size - urls.uniq.size).to eq 0
+      expect(@urls.size - @urls.uniq.size).to eq 0
     end
   end
 
   describe "omitted url" do
 
     describe "one time redirect" do
-      tco_url = "https://t.co/DyfjrVazCG"
-      original_url = "https://speakerdeck.com/kkabetani/kanazawa-dot-rb-slides"
+      let(:omitted_url)  { "https://t.co/DyfjrVazCG" }
+      let(:original_url) { "https://speakerdeck.com/kkabetani/kanazawa-dot-rb-slides" }
 
       it "convert to correct original url" do
-        expect(KanazawarbSlides.convert_to_original_url(tco_url)).to eq original_url
+        expect(KanazawarbSlides.convert_to_original_url(omitted_url)).to eq original_url
       end
     end
 
     describe "many times redirect" do
-      tco_url = "http://t.co/qPedglEadF"
-      original_url = "http://www.slideshare.net/tomokazu/textobjects-vims-elegant-function"
+      let(:omitted_url)  { "http://t.co/qPedglEadF" }
+      let(:original_url) { "http://www.slideshare.net/tomokazu/textobjects-vims-elegant-function" }
 
       it "convert to correct original url" do
-        expect(KanazawarbSlides.convert_to_original_url(tco_url)).to eq original_url
+        expect(KanazawarbSlides.convert_to_original_url(omitted_url)).to eq original_url
       end
 
       it "raise exception for too deep" do
-        expect { KanazawarbSlides.convert_to_original_url(tco_url, 1) }.to raise_error(ArgumentError)
+        expect { KanazawarbSlides.convert_to_original_url(omitted_url, 1) }.to raise_error(ArgumentError)
       end
     end
 
     describe "not found response" do
-      tco_url = "http://kanazawarb.github.io/meetup/notfound.html"
+      let(:omitted_url) { "http://kanazawarb.github.io/meetup/notfound.html" }
 
       it "return nil" do
-        expect(KanazawarbSlides.convert_to_original_url(tco_url)).to eq nil
+        expect(KanazawarbSlides.convert_to_original_url(omitted_url)).to eq nil
       end
     end
   end
